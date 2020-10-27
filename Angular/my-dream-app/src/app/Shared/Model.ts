21f6646e8b0ad50d0f942f119d0e601f1778ad4f
Александр Петроченко <e44alex@gmail.com>
2020-10-27 13:02:45 +0300

    changed model

--- Angular/my-dream-app/src/app/Shared/Model.ts
+++ Angular/my-dream-app/src/app/Shared/Model.ts
@@ -1,22 +1,36 @@
 import { DecimalPipe } from '@angular/common';
+import { Guid } from 'guid-typescript';
 
 export class User {
-    Id: string;
+    Id: Guid;
     username: string;
     email:string;
     rating: number;
 }
 
 export class Question{
+    Id: Guid;
     topic: string;
     body: string;
     dateCreated: Date;
     lastActivity: Date;
-    creator: User
+    creator: User;
+    opened: boolean;
+    answers: Answer[]
 }
 
 export class Answer{
+    Id: Guid;
+    QuestionId: Guid;
+    CreatorId: Guid;
     body:string;
     dateCreated: string;
+    creator: User;
+    likers: User[];
+}
 
+export class AnswerLiker{
+    Id: Guid;
+    user: User;
+    answer: Answer
 }
